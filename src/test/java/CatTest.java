5e53baf7826f208c1c9594ef8e26e7466e845674
DmitryChebokhin <dmitrytchebohin@yandex.ru>
2024-08-20 13:45:37 +0300

    Changes 1

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -21,14 +21,13 @@ public class CatTest {
         Cat cat = new Cat(feline);
         String actual = cat.getSound();
         String expected = "Мяу";
-        assertEquals(expected, actual);
+        assertEquals("Коты так не говорят", expected, actual);
     }
     @Test
     public void getFoodTest() throws Exception {
         Cat cat = new Cat(feline);
-        Mockito.when(feline.eatMeat()).thenReturn(List.of("Животные", "Птицы", "Рыба"));
         List<String> expectedResult = List.of("Животные", "Птицы", "Рыба");
-        List<String> actualResult = cat.getFood();
-        assertEquals(expectedResult, actualResult);
+        Mockito.when(feline.eatMeat()).thenReturn(expectedResult);
+        assertEquals("Не тот печерень еды", expectedResult, cat.getFood());
     }
 }
